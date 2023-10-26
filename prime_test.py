22f15a1ff4eb976bc4e2aef0eb0a25d88bd7e60f
MusiliC <musilibrian07@gmail.com>
2023-10-26 16:48:45 +0300

    Fixing test file

--- prime_test.py
+++ prime_test.py
@@ -3,5 +3,5 @@ from prime import is_prime
 
 def test_prime_number():
     # Test if a prime number (e.g., 7) is identified as prime
-    result = is_prime(8)
+    result = is_prime(7)
     assert result is True
